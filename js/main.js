1cbb5cfc40a708791d878b1fda90e5275a34ba3a
Caleb Clayson <clayson.caleb@gmail.com>
2018-11-07 08:11:44 -0700

    moved over to the api now that it is running all info displayed to page besides scoring player name edit functional

--- js/main.js
+++ js/main.js
@@ -1,79 +1,117 @@
-let data;
-let http = new XMLHttpRequest();
+var mycourses;
+var mycourse;
+let currentTeeIndex;
+let numplayers = 1;
 
-const fetchData = new Promise(
-    () => {
-        http.open('GET', '../data/test.json');
-        http.send();
-    }
-);
+(function () {
+    $.ajax({
+        url: 'https://golf-courses-api.herokuapp.com/courses',
+        type: 'GET',
+        success: response => {
+            mycourses = response.courses;
+            loadDoc();
+        }
+    })
+})();
 
-http.onreadystatechange = () => {
-    if(http.readyState == XMLHttpRequest.DONE) {
-        data = JSON.parse(http.response);
-    }
-}
-
-function displayDataToPage() {
-    displayTees();
-    displayHoles();
-    displayHandicap();
-    displayPar();
-    displayYardage();
-}
-
-function displayTees() {
-    let tees = data.course.tee_types;
-    for(let i = 0; i < tees.length; i++) {
-        let teeName = tees[i].tee_type;
-        let teeOption = `<option value="${teeName}">${teeName}</option>`;
-        $('#tees').append(teeOption);
+function loadDoc() {
+    for (let i = 0; i < mycourses.length; i++) {
+        $('.courses').append(`<option value="${mycourses[i].id}"> ${mycourses[i].name}</option>`);
     }
 }
 
-let teeIndex = () => {
-    if($('#tees').val() === 'pro') {
-        return 0;
-    }else if ($('#tees').val() === 'champion') {
-        return 1;
-    }else if($('#tees').val() === 'men') {
-        return 2;
-    }else if($('#tees').val() === 'women') {
-        return 3;
-    }
-};
+function loadCourse(courseId) {
+    $.ajax({
+        url: `https://golf-courses-api.herokuapp.com/courses/${courseId}`,
+        type: 'GET',
+        success: response => {
+            mycourse = response.data;
+            displayHoleColumns();
+            
+            $('.tees').html('');
+            let teeArray = mycourse.holes[0].teeBoxes;
+            for (let i = 0; i < teeArray.length; i++) {
+                $('.tees').append(`<option value="${i}">${teeArray[i].teeType}</option>`)
+            }
+        }
+    })
+    $('.tees').css('display', 'inline');
+}
 
-function displayHoles() {
-    let holes = data.course.holes;
-    for(let i = 0; i < holes.length; i++) {
-        let holeDiv = `<div class="hole">${holes[i].hole_num}</div>`;
-        $('.hole-row').append(holeDiv);
+function chooseTee(e) {
+    currentTeeIndex = Number(e);
+    addHoles();
+}
+
+function displayHoleColumns() {
+    $('.grid-container').html('');
+    for (let i = 0; i < mycourse.holes.length; i++) {
+        $('.grid-container').append(`
+            <div id="col${i + 1}" class="column"><div class="hole-label">Hole ${i + 1}<div></div>
+        `);
     }
 }
 
-function displayHandicap() {
-    let holes = data.course.holes;
-    for(let i = 0; i < holes.length; i++) {
-        let hcp = holes[i].tee_boxes[teeIndex()].hcp;
-        let hcpDiv = `<div class="hcp">${hcp}</div>`;
-        $('.handicap-row').append(hcpDiv);
+function addYards() {
+    for (let i = 0; i < mycourse.holes.length; i++) {
+        $(`#col${i + 1}`).append(`
+            <div class="yard-label">${mycourse.holes[i].teeBoxes[currentTeeIndex].yards} yrds</div>
+        `);
     }
 }
 
-function displayPar() {
-    let holes = data.course.holes;
-    for(let i = 0; i < holes.length; i++) {
-        let par = holes[i].tee_boxes[teeIndex()].par;
-        let parDiv = `<div class="par">${par}</div>`;
-        $('.par-row').append(parDiv);
+function addPar() {
+    for (let i = 0; i < mycourse.holes.length; i++) {
+        $(`#col${i + 1}`).append(`
+            <div class="yard-label">Par is ${mycourse.holes[i].teeBoxes[currentTeeIndex].par}</div>
+        `);
     }
 }
 
-function displayYardage() {
-    let holes = data.course.holes;
-    for(let i = 0; i < holes.length; i++) {
-        let yardage = holes[i].tee_boxes[teeIndex()].yards;
-        let yardDiv = `<div class="yard">${yardage}</div>`;
-        $('.yardage-row').append(yardDiv);
+function addHCP() {
+    for (let i = 0; i < mycourse.holes.length; i++) {
+        $(`#col${i + 1}`).append(`
+            <div class="yard-label">Handicap ${mycourse.holes[i].teeBoxes[currentTeeIndex].hcp}</div>
+        `);
     }
+}
+
+function addHoles() {
+    for (let p = 1; p <= numplayers; p++) {
+        for (let h = 0; h <= mycourse.holes.length; h++) {
+            $(`#col${h + 1}`).html('');
+        }
+    }
+    for (let i = 0; i < mycourse.holes.length; i++) {
+        $(`#col${i + 1}`).append(`
+            <div class="hole-label">Hole ${i + 1}<div>
+        `);
+    }
+    for (let p = 1; p <= numplayers; p++) {
+        for (let h = 0; h <= mycourse.holes.length; h++) {
+            $(`#col${h + 1}`).append(`<label class="player-wrapper"><span class="name${p}">Player ${p}</span><i class="fas fa-pen" onclick="changeName(this)"></i><input type="text" id="p${p}h${h + 1}"/></label>`);
+        }
+    }
+    for (let h = 0; h <= mycourse.holes.length; h++) {
+        $(`#col${h + 1}`).append(`<div>Info</div>`);
+    }
+    addYards();
+    addPar();
+    addHCP();
+    $('.addPlayer').css('display', 'inline');
+}
+
+function addPlayer() {
+    numplayers += 1;
+    addHoles();
+}
+
+function totals() {
+
+}
+
+function changeName(e) {
+    let newName = prompt('what do you want for your new name?');
+    let nameLabel = $(e).parent().find("span");
+    $(`.${nameLabel.attr("class")}`).html(newName);
 }
